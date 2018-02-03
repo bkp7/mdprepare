20b0d45cdeefdda012d069d03bcd2367977dc4aa
bkp7 <barry@x-ware.co.uk>
2018-02-03 13:55:16 +0000

    Added code and unit tests for finding code in markdown

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,12 @@
-const debug = require('debug')('mdprepare:helpers')
+/** findComment and findCode functions use a similar layout to return the location and contents
+  *   .start          => points at the character in the string why the other item starts (ie. comment or code block)
+  *   .length         => is the overall length of the comment or code block.
+  *   .internalStart  => points at the character in the string where the internal payload starts
+  *   .internalLength => is the length of the internal payload
+  *   .commandString  => is the command string found within the particular item
+  *   .info           => is a structure containing further info about what was found
+  * if start is set to -1 then nothing was found
+**/
 
 const commentStyles = [
   {opening: '<!---', closings: ['-->']},
@@ -21,13 +29,11 @@ export function findComment (txt, start) {
     p = txt.indexOf(commentStyles[i].opening, start)
     if ((p !== -1) && (p < r.start)) { r.start = p; style = i }
   }
-  debug('style: ' + style + ', r.start: ' + r.start)
   if (style === -1) {
     // we did not find any comments
     r.start = -1
     return r
   }
-  debug(commentStyles[style])
   r.internalStart = r.start + commentStyles[style].opening.length
 
   // now find the corresponding end of the comment
@@ -46,4 +52,217 @@ export function findComment (txt, start) {
     r.length = p + commentStyles[style].closings[i].length - r.start
   }
   return r
+}
+
+export function findCode (txt, start) {
+  /**
+  * finds the next code in the string provided starting at position start
+  * returns an object containing start, length, internalStart, internalLength
+  *
+  * there are three types of code insertion - code span (inline), fenced code and indented code
+  *
+  * eg. span (1 or more backticks):
+  * some text ``echo myfile.txt`` more text
+  *
+  * eg. indented (4 or more indent spaces):
+  * some text
+  *     function test() {
+  *       console.log('test')
+  *     }
+  * more text
+  *
+  * eg. fenced (3 or more backticks on a row on their own)
+  * some text
+  * ``` js
+  * function test() {
+  *   console.log('test')
+  * }
+  * ```
+  * more text
+  *
+  **/
+  let x = _findFencedCode(txt, start)
+  let y = _findIndentedCode(txt, start)
+  let z = _findCodeSpan(txt, start)
+
+  return _earlierOf(x, _earlierOf(y, z))
+
+  function _earlierOf (a, b) {
+    // inspects the .start property of a and b and returns the one
+    // with the lowest start position
+    if (b.start !== -1 && (a.start === -1 || b.start < a.start)) {
+      return b
+    } else {
+      return a
+    }
+  }
+}
+
+function _findCodeSpan (txt, start) {
+  // finds an inline Code Span in the format: 'some text ``echo myfile.txt`` more text'
+  // look for start
+  let lookFrom = start
+  while (true) {
+    let s = _findCodeSpanStart(txt, lookFrom)
+    if (s.start === -1) { return s }
+    // look for end
+    let e = _findCodeSpanEnd(txt, s)
+    if (e.start !== -1) { return e }
+    lookFrom = s.internalStart
+  }
+
+  function _findCodeSpanStart (txt, start) {
+    let regex = /(^|[^`])(`+)[^`]/g
+    // 1st capture group is the first (or no) character prior to the identifying `'s
+    // 2nd group is the ` characters (however many there are)
+    regex.lastIndex = start
+    let regexResult = regex.exec(txt)
+    if (regexResult === null) { return {start: -1} }
+    let preLen = 0
+    if (regexResult[1] !== undefined) { preLen = regexResult[1].length }
+    let r = {
+      start: regexResult.index + preLen,
+      internalStart: regexResult.index + preLen + regexResult[2].length,
+      info: {
+        codeFence: regexResult[2]
+      }
+    }
+    return r
+  }
+
+  function _findCodeSpanEnd (txt, opening) {
+    let r = JSON.parse(JSON.stringify(opening)) // create copy of opening structure passed in
+    let regex = RegExp('([^`])(' + r.info.codeFence + ')($|[^`])', 'g')
+    regex.lastIndex = r.internalStart
+    let regexResult = regex.exec(txt)
+    if (regexResult === null) { return {start: -1} }
+    let postLen = 0
+    if (regexResult[3] !== undefined) { postLen = regexResult[3].length }
+    r.internalLength = regexResult.index + regexResult[1].length - r.internalStart
+    r.length = regexResult.index + regexResult[0].length - postLen - r.start
+    r.commandString = ''
+    return r
+  }
+}
+
+function _findIndentedCode (txt, start) {
+  let regex = /((?:^|\n)[ ]{4,}[^\r\n\0]*){1,}/
+  regex.lastIndex = start
+  let regexResult = regex.exec(txt)
+  if (regexResult === null) {
+    return {start: -1}
+  } else {
+    return {
+      start: regexResult.index,
+      length: regexResult[0].length,
+      internalStart: regexResult.index,
+      internalLength: regexResult[0].length,
+      info: {indent: regexResult[2]},
+      commandString: ''
+    }
+  }
+}
+
+function _findFencedCode (txt, start) {
+  // if the internalLength is returned as -1 this means that text cannot simply be inserted at the internalStart
+  // location. Instead an additional preceding new line must be inserted along with the new text
+  // another way to look at this is that the internal text is 1 character short
+  // a value of -2 indicates a CRLF needs to be inserted
+  let a = _findOpeningCodeFence(txt, start)
+  if (a.start === -1) { return a }
+  return _findClosingCodeFence(txt, a)
+
+  function _findOpeningCodeFence (txt, start) {
+    // returns the location and type of the next opening code fence
+    let regex = /^([ ]{0,3}> |>|[ ]{0,0})(([ ]{0,3})([`]{3,}|[~]{3,})([^\n\r\0`]*))$/m
+    /** The regex groups are:
+      * 0: the full match including any preamble block markup
+      * 1: the preamble consisting of block characters or nothing
+      * 2: the full codeFence line without preamble
+      * 3: any leading blank spaces at the start of the codeFence line
+      * 4: the ` or ~ characters identifying the codeFence
+      * 5: anything else on the line following the codeFence
+    **/
+    regex.lastIndex = start
+    let regexResult = regex.exec(txt)
+    if (regexResult === null) {
+      return {start: -1}
+    }
+    let r = { start: regexResult.index,
+      info: {
+        blockQuote: regexResult[1],
+        spacesCount: regexResult[3].length,
+        codeFence: regexResult[4]
+      },
+      commandString: regexResult[5].trim(),
+      internalStart: regexResult.index + regexResult[0].length
+    }
+    if (txt[r.internalStart] === '\r') { r.internalStart ++ }
+    if (txt[r.internalStart] === '\n') { r.internalStart ++ }
+    return r
+  }
+
+  function _findClosingCodeFence (txt, opening) {
+    // updates the passed result structure with the location and type of the next closing code fence
+    // to match the opening cofeFEnce passed in
+    let regex
+    let r = JSON.parse(JSON.stringify(opening)) // create copy of opening structure passed in
+    regex = RegExp('^[ ]{0,3}[' + r.info.codeFence[0] + ']{' + r.info.codeFence.length + ',}[ ]*$', 'mg')
+    regex.lastIndex = r.internalStart
+    let regexResult = regex.exec(txt)
+    if (opening.info.blockQuote.length !== 0) {
+      // we are in a block quote so the codeFence will end at the earlier of the found regex OR end of the block quote
+      let b = _findEndOfBlock(txt, r.internalStart)
+      if (b !== -1 && (regexResult === null || b < regexResult.index)) {
+        // the block end dictates the code block end
+        r.internalLength = b - r.internalStart
+        r.length = b - r.start
+        return r
+      }
+    }
+    if (regexResult === null) {
+      r.internalLength = txt.length - r.internalStart
+      r.length = txt.length - r.start
+    } else {
+      r.internalLength = regexResult.index - r.internalStart
+      if (txt[r.internalStart + r.internalLength - 1] === '\n') { r.internalLength -- }
+      if (txt[r.internalStart + r.internalLength - 1] === '\r') { r.internalLength -- }
+      r.length = regexResult.index + regexResult[0].length - r.start
+    }
+    return r
+  }
+
+  function _findEndOfBlock (txt, start) {
+    // finds the first line which is not marked as block
+    let regex = /\n(?!([ ]{0,3}> |>))[^\n]*/
+    regex.lastIndex = start
+    let regexResult = regex.exec(txt)
+    if (regexResult === null) {
+      return -1
+    } else {
+      return regexResult.index
+    }
+  }
+}
+
+export function _findEarliestOf (txt, start, arrPossibles) {
+  /** returns both the location of and the index within the arr of the first match it can find
+    * eg. _findEarliestOf('a test string ``` <¬-- here', 5, ['~~~', '```', '<---'])
+    * will return [14, 1]
+    * if no matches are found it will return -1 as the position
+    * if 2 strings in the array match it will return the array index of the first one that matches
+  **/
+  let arrIndex = 0
+  let len = txt.length
+  let pos = txt.length + 1
+  let posTemp = -1
+  for (let i = 0; i < len; i++) {
+    posTemp = txt.indexOf(arrPossibles[i], start)
+    if (posTemp !== -1 && posTemp < pos) {
+      pos = posTemp
+      arrIndex = i
+    }
+  }
+  if (pos === len + 1) { pos = -1 }
+  return [pos, arrIndex]
 }
--- test/test.js
+++ test/test.js
@@ -4,9 +4,55 @@
 // import mdprepare from '../dist/mdprepare.js'
 // import {findComment} from '../dist/helpers.js'
  const processFile = require('../dist/processFile.js').default
- const findComment = require('../dist/helpers.js').findComment
+ const {findComment, _findEarliestOf, findCode} = require('../dist/helpers.js')
  const assert = require('assert')
  const {exec} = require('child_process')
+
+ const fencedCodeTests = [
+   {name: 'Example 88', text: '```\n<\n >\n```', result: {start: 0, length: 12, internalStart: 4, internalLength: 4, commandString: ''}},
+   {name: 'Example 89', text: '~~~\n<\n >\n~~~', result: {start: 0, length: 12, internalStart: 4, internalLength: 4, commandString: ''}},
+   {name: 'Example 90', text: '``\nfoo\n``', result: {start: 0, length: 9, internalStart: 2, internalLength: 5, commandString: ''}},
+   {name: 'Example 91', text: '```\naaa\n~~~\n```', result: {start: 0, length: 15, internalStart: 4, internalLength: 7, commandString: ''}},
+   {name: 'Example 92', text: '~~~\naaa\n```\n~~~', result: {start: 0, length: 15, internalStart: 4, internalLength: 7, commandString: ''}},
+   {name: 'Example 93', text: '````\naaa\n```\n``````', result: {start: 0, length: 19, internalStart: 5, internalLength: 7, commandString: ''}},
+   {name: 'Example 94', text: '~~~~\naaa\n~~~\n~~~~', result: {start: 0, length: 17, internalStart: 5, internalLength: 7, commandString: ''}},
+   {name: 'Example 95', text: '```', result: {start: 0, length: 3, internalStart: 3, internalLength: 0, commandString: ''}},
+   {name: 'Example 96', text: '`````\n\n```\naaa', result: {start: 0, length: 14, internalStart: 6, internalLength: 8, commandString: ''}},
+   {name: 'Example 97', text: '> ```\n> aaa\n\nbbb', result: {start: 0, length: 11, internalStart: 6, internalLength: 5, commandString: ''}},
+   {name: 'Example 98', text: '```\n\n  ```', result: {start: 0, length: 10, internalStart: 4, internalLength: 0, commandString: ''}},
+   {name: 'Example 99', text: '```\n```', result: {start: 0, length: 7, internalStart: 4, internalLength: -1, commandString: ''}},
+   {name: 'Example 100', text: ' ```\n aaa\naaa\n```', result: {start: 0, length: 17, internalStart: 5, internalLength: 8, commandString: ''}},
+   {name: 'Example 101', text: '  ```\naaa\n  aaa\naaa\n  ```', result: {start: 0, length: 25, internalStart: 6, internalLength: 13, commandString: ''}},
+   {name: 'Example 102', text: '   ```\n   aaa\n    aaa\n   ```', result: {start: 0, length: 28, internalStart: 7, internalLength: 14, commandString: ''}},
+   {name: 'Example 103', text: '    ```\n    aaa\n    ```', result: {start: 0, length: 23, internalStart: 0, internalLength: 23, commandString: ''}},
+   {name: 'Example 104', text: '```\naaa\n  ```', result: {start: 0, length: 13, internalStart: 4, internalLength: 3, commandString: ''}},
+   {name: 'Example 105', text: '   ```\naaa\n  ```', result: {start: 0, length: 16, internalStart: 7, internalLength: 3, commandString: ''}},
+   {name: 'Example 106', text: '```\naaa\n    ```', result: {start: 0, length: 15, internalStart: 4, internalLength: 11, commandString: ''}},
+   {name: 'Example 107', text: '``` ```\naaa', result: {start: 0, length: 7, internalStart: 3, internalLength: 1, commandString: ''}},
+   {name: 'Example 108', text: '~~~~~~\naaa\n~~~ ~~', result: {start: 0, length: 17, internalStart: 7, internalLength: 10, commandString: ''}},
+   {name: 'Example 109', text: 'foo\n```\nbar\n```\nbaz', result: {start: 4, length: 11, internalStart: 8, internalLength: 3, commandString: ''}},
+   {name: 'Example 110', text: 'foo\n---\n~~~\nbar\n~~~\n# baz', result: {start: 8, length: 11, internalStart: 12, internalLength: 3, commandString: ''}},
+   {name: 'Example 111', text: '```ruby\ndef foo(x)\n  return 3\nend\n```', result: {start: 0, length: 37, internalStart: 8, internalLength: 25, commandString: 'ruby'}},
+   {name: 'Example 112', text: '~~~~    ruby startline=3 $%@#$\r\ndef foo(x)\r\n  return 3\r\nend\r\n~~~~~~~', result: {start: 0, length: 68, internalStart: 32, internalLength: 27, commandString: 'ruby startline=3 $%@#$'}},
+   {name: 'Example 113', text: '````;\r\n````', result: {start: 0, length: 11, internalStart: 7, internalLength: -2, commandString: ';'}},
+   {name: 'Example 114', text: '``` aa ```\r\nfoo', result: {start: 0, length: 10, internalStart: 3, internalLength: 4, commandString: ''}},
+   {name: 'Example 115', text: '```\r\n``` aaa\r\n```', result: {start: 0, length: 17, internalStart: 5, internalLength: 7, commandString: ''}},
+   {name: 'Example 324', text: '`foo`', result: {start: 0, length: 5, internalStart: 1, internalLength: 3, commandString: ''}},
+   {name: 'Example 325', text: '`` foo ` bar  ``', result: {start: 0, length: 16, internalStart: 2, internalLength: 12, commandString: ''}},
+   {name: 'Example 326', text: '` `` `', result: {start: 0, length: 6, internalStart: 1, internalLength: 4, commandString: ''}},
+   {name: 'Example 327', text: '``\r\nfoo\r\n``', result: {start: 0, length: 11, internalStart: 2, internalLength: 7, commandString: ''}},
+   {name: 'Example 328', text: '`foo   bar\r\n  baz`', result: {start: 0, length: 18, internalStart: 1, internalLength: 16, commandString: ''}},
+   {name: 'Example 329', text: '`a b`', result: {start: 0, length: 5, internalStart: 1, internalLength: 3, commandString: ''}},
+   {name: 'Example 330', text: '`foo `` bar`', result: {start: 0, length: 12, internalStart: 1, internalLength: 10, commandString: ''}},
+   {name: 'Example 331', text: '`foo\\`bar`', result: {start: 0, length: 6, internalStart: 1, internalLength: 4, commandString: ''}},
+   {name: 'Example 332', text: '*foo`*`', result: {start: 4, length: 3, internalStart: 5, internalLength: 1, commandString: ''}},
+   {name: 'Example 333', text: '[not a `link](/foo`)', result: {start: 7, length: 12, internalStart: 8, internalLength: 10, commandString: ''}},
+   {name: 'Example 334', text: '`<a href="`">`', result: {start: 0, length: 11, internalStart: 1, internalLength: 9, commandString: ''}},
+   {name: 'Example 336', text: '`<http://foo.bar.`baz>`', result: {start: 0, length: 18, internalStart: 1, internalLength: 16, commandString: ''}},
+   {name: 'Example 338', text: '```foo``', result: {start: -1}},
+   {name: 'Example 339', text: '`foo', result: {start: -1}},
+   {name: 'Example 340', text: '`foo``bar``', result: {start: 4, length: 7, internalStart: 6, internalLength: 3, commandString: ''}}
+ ]
 
  describe('Dummy test', function () {
    it('1 should equal 1', function () {
@@ -81,6 +127,29 @@
        it('finds comment style (2B) [//]: <> (comment)\n', function () {
          assert.deepEqual(findComment('test[//]: <> (comment)\nend', 0), {start: 4, length: 19, internalStart: 14, internalLength: 7})
        })
+     })
+     describe('_findEarliestOf', function () {
+       it('target in middle of string', function () {
+         assert.deepEqual(_findEarliestOf('a test string ``` <¬-- here', 5, ['~~~', '```', '<---']), [14, 1])
+       })
+       it('target at beginning of string', function () {
+         assert.deepEqual(_findEarliestOf('a test string ``` <¬-- here', 0, ['~~~', '````', '<!---', 'a ']), [0, 3])
+       })
+       it('target at end of string', function () {
+         assert.deepEqual(_findEarliestOf('a test string ``` <¬-- hire', 18, ['~~~', '```', '<!---', 'e']), [26, 3])
+       })
+       it('target not in string', function () {
+         assert.deepEqual(_findEarliestOf('a test string ``` <¬-- here', 5, ['~~~', '````', '<!---']), [-1, 0])
+       })
+     })
+     describe('findCode', function () {
+       for (let i = 0; i < fencedCodeTests.length; i++) {
+         it(fencedCodeTests[i].name, function () {
+           let r = findCode(fencedCodeTests[i].text)
+           delete r.info
+           assert.deepEqual(r, fencedCodeTests[i].result)
+         })
+       }
      })
    })
    describe('processFile.js', function () {
